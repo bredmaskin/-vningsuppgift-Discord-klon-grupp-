1886157bef4a3bc30bf7d7eaf295324b81dc56fc
SebastianJ-5 <sebastian.johansson.net25@edu.edugrade.se>
2026-03-05 13:14:32 +0100

    Added GET and POST for messages

---
+++ ChatMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace _vningsuppgift_Discord_klon_grupp_
+{
+    public class ChatMessage
+    {
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,28 @@
+﻿using _vningsuppgift_Discord_klon_grupp_;
+using System.Text.Json;
+
+var messages = new List<ChatMessage>
+{
+    new ChatMessage { User = "Alice", Message = "Hej!" },
+    new ChatMessage { User = "Bob", Message = "Tjena!" }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
+
+app.MapGet("/api/messages", () =>
+{
+    return Results.Json(messages);
+});
+
+app.MapPost("/api/messages", (ChatMessage newMessage) =>
+{
+    messages.Add(newMessage);
+    return Results.Created($"/api/messages/{newMessage.User}", newMessage);
+});
 
 app.Run("http://localhost:3000");
