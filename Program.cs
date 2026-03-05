20a5c987fcbb6c884c23b85d712c653253e7315b
bredmaskin <patrikbredin@hotmail.se>
2026-03-05 10:40:02 +0100

    Middleware

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 var app = WebApplication.Create(args);
 app.MapGet("/", () => "Hello World!");
+
+// Middleware
+app.UseCors("");
+
+// Servera statiska filer från wwwroot (index.html, css, js)
+app.UseDefaultFiles();  // index.html som standardfil
+app.UseStaticFiles();   // css, js, bilder
+app.UseAuthorization();
+
 app.Run("http://localhost:3000");
